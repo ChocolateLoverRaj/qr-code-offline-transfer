40e7bc7b2200eebe7d4da349ef8968ac5eea36dc
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-09-23 11:35:53 -0700

    Offline PWA

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
+// @ts-ignore
+import iconsJson from './icons.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        theme_color: '#FFFFFF',
+        background_color: '#FF000000',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        short_name: 'QR Share',
+        description: 'Send data offline through QR codes and scanning it with a camera',
+        name: 'QR Code Offline Transfer',
+        icons: iconsJson.icons
+      }
+    })
+  ]
+})
